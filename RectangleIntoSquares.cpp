a17ee82485b4d03727ba4dadc02366b23d0f0e29
sbwhitehouse <47610872+sbwhitehouse@users.noreply.github.com>
2020-02-07 14:04:27 -0700

    Removed extra var

--- RectangleIntoSquares.cpp
+++ RectangleIntoSquares.cpp
@@ -11,17 +11,14 @@ class SqInRect
     //Variables to track the necessary attributes of the rectangle
     int area = lng * wdth;
     int long_side = std::max(lng,wdth);
-    int prev_long_side = long_side;
     int short_side = std::min(lng,wdth);
     for(int i = short_side; i > 0; i = short_side) {
         if(area - (i * i) >= 0) {
-            //Track the long side 1 square back
-            prev_long_side = long_side;
             //Only change the short_side if it is actually decreasing
             if(short_side >= long_side - short_side) {
                short_side = long_side - short_side;
             }
-            long_side = prev_long_side - short_side;
+            long_side = long_side - short_side;
             squares.push_back(i);
             area -= (i * i);
         }
